433a551d1bb3756a669b74913cda89fd2790d826
gk <gkdtu@gmail.com>
2020-05-15 08:49:35 +0530

    Updates

---
+++ precache-manifest.56c317703a432be193f8284dac6750b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490b85b173834c7fb0671bb462a13bf0",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "e6cd9b722001c182a62c",
+    "url": "/smart-brain/static/css/2.8cf49568.chunk.css"
+  },
+  {
+    "revision": "3950b362a0538ce0ad58",
+    "url": "/smart-brain/static/css/main.2eb5e72c.chunk.css"
+  },
+  {
+    "revision": "e6cd9b722001c182a62c",
+    "url": "/smart-brain/static/js/2.77348930.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/smart-brain/static/js/2.77348930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3950b362a0538ce0ad58",
+    "url": "/smart-brain/static/js/main.be9e7c4f.chunk.js"
+  },
+  {
+    "revision": "8a850920e7243316a5ed",
+    "url": "/smart-brain/static/js/runtime-main.96a5324c.js"
+  }
+]);
